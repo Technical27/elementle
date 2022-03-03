9604512240fb7bf5837d9689b7d4bd0692f1ed65
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2022-03-03 08:21:57 -0500

    minor fixes and add table as a hint

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import order from "./assets/order.json";
   import { DateTime } from "luxon";
   import Answer from "./lib/Answer.svelte";
+  import Table from "./lib/Table.svelte";
 
   // If we forget to change the order, it will just loop
   const currentDay =
@@ -16,7 +17,8 @@
 
   let currentAttempt = 0;
 
-  let hints = [0, 0, 0, 0, 0];
+  let answerHints = new Array(5).fill(0);
+  let tableHints = new Array(118).fill(0);
   let previousGuesses = [];
 
   const guess = (m) => {
@@ -27,25 +29,52 @@
     if (i === -1) return;
 
     if (i === currentIndex) {
-      hints[currentAttempt] = -1;
+      answerHints[currentAttempt] = -1;
+      tableHints[i] = -1;
       currentAttempt = 5;
     } else {
       previousGuesses.push(guess);
       const hint = i > currentIndex ? i - currentIndex : currentIndex - i;
-      hints[currentAttempt] = hint;
+      // Put a hint by the current guess
+      answerHints[currentAttempt] = hint;
+      // Put a hint on the periodic table
+      tableHints[i] = hint;
       currentAttempt++;
     }
   };
 </script>
 
 <main>
-  <h1>Elementle</h1>
-  <div id="answer-grid">
-    <Answer enabled={currentAttempt === 0} on:guess={guess} hint={hints[0]} />
-    <Answer enabled={currentAttempt === 1} on:guess={guess} hint={hints[1]} />
-    <Answer enabled={currentAttempt === 2} on:guess={guess} hint={hints[2]} />
-    <Answer enabled={currentAttempt === 3} on:guess={guess} hint={hints[3]} />
-    <Answer enabled={currentAttempt === 4} on:guess={guess} hint={hints[4]} />
+  <div>
+    <h1>Elementle</h1>
+    <Table hints={tableHints} />
+    <div id="answer-grid">
+      <Answer
+        enabled={currentAttempt === 0}
+        on:guess={guess}
+        hint={answerHints[0]}
+      />
+      <Answer
+        enabled={currentAttempt === 1}
+        on:guess={guess}
+        hint={answerHints[1]}
+      />
+      <Answer
+        enabled={currentAttempt === 2}
+        on:guess={guess}
+        hint={answerHints[2]}
+      />
+      <Answer
+        enabled={currentAttempt === 3}
+        on:guess={guess}
+        hint={answerHints[3]}
+      />
+      <Answer
+        enabled={currentAttempt === 4}
+        on:guess={guess}
+        hint={answerHints[4]}
+      />
+    </div>
   </div>
   <footer>
     Created by Aamaruvi Yogamani and inspired by the original Wordle game.
@@ -55,7 +84,9 @@
 </main>
 
 <style>
-  :root {
+  :root,
+  :global(div#app),
+  :global(body) {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
     margin: 0;
@@ -63,18 +94,23 @@
     border: 0;
     height: 100vh;
     width: 100vw;
+    min-height: 430px;
   }
 
   main {
     text-align: center;
-    padding: 1em;
+    display: flex;
+    height: 100%;
+    flex-flow: column nowrap;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+
+  main > div {
     margin: 0 auto;
   }
 
   footer {
-    position: absolute;
-    bottom: 0;
-    left: 0;
     width: 100%;
     text-align: center;
     font-size: 0.8em;
@@ -84,7 +120,6 @@
 
   #answer-grid {
     display: grid;
-    margin: 0 auto;
     width: 40em;
     grid-template: repeat(5, 4em) / auto;
   }
